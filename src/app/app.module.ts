c30ef575ad47d28baa4ece0265686ed78a7cbc80
Karuna Sharma <karuna.sharma@nsi.lab>
2018-07-10 18:04:54 +0530

    routing and two way binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { routing } from './app.routing';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainContentComponent } from './main-content/main-content.component';
+import { AboutComponent } from './about/about.component';
+import { LandingComponent } from './landing/landing.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainContentComponent,
+    AboutComponent,
+    LandingComponent,
+    SidebarComponent,
+    ContactComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+	FormsModule,
+	routing
   ],
   providers: [],
   bootstrap: [AppComponent]
